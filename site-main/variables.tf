94fea1f534c50480f45b5dd96b6f88aeb6741dde
Rahul Sharma <er.csrahul@gmail.com>
2019-01-17 12:39:57 +0530

    Update variables.tf

--- site-main/variables.tf
+++ site-main/variables.tf
@@ -52,3 +52,22 @@ variable "viewer_protocol_policy" {
   description = "CloudFront default viewer"
   default     = "allow-all"
   }
+variable "cors_allowed_headers" {
+  default = ["Authorization"]
+}
+
+variable "cors_allowed_methods" {
+  default = ["GET"]
+}
+
+variable "cors_allowed_origins" {
+  default = ["*"]
+}
+
+variable "cors_expose_headers" {
+  default = []
+}
+
+variable "cors_max_age_seconds" {
+  default = "3000"
+}
